4722377281ca434c6101b8c1961ac94271c963c6
Lee <a_lee100712@ats-hr420-dm31.fc.ca>
2020-02-28 12:45:25 -0500

    updatingsoundpack

--- js/main.js
+++ js/main.js
@@ -84,6 +84,23 @@
             }
 
             // create and play explosion sound
+                    // create and play cheesy laser sound
+    let audio = document.createElement('audio');
+
+    // create an <audio></audio> element
+    
+    audio.addEventListener("ended", function() {
+      document.body.removeChild(audio);
+
+    });
+    audio.src = "audio/explode.mp3"; // set the audio source
+
+    document.body.appendChild(audio); // add it to the page
+
+    audio.play();
+    
+  
+            //end explosion sound
             
           }
         });
@@ -137,6 +154,19 @@
     bullets.push(newBullet);
 
     // create and play cheesy laser sound
+    let audio = document.createElement('audio');
+
+    // create an <audio></audio> element
+    
+    audio.addEventListener("ended", function() {
+      document.body.removeChild(audio);
+
+    });
+    audio.src = "audio/laser.mp3"; // set the audio source
+
+    document.body.appendChild(audio); // add it to the page
+
+    audio.play();
     
   }
 
